dcb40243e33083dec63bbf9d303ed38db11d4598
Koya Nishio <foxynoxy07@gmail.com>
2025-09-30 16:03:46 +0200

    Added Class/Objects(Art)

---
+++ src/main/AssetSetter.java
@@ -0,0 +1,24 @@
+package main;
+
+import object.OBJ_Key;
+
+public class AssetSetter {
+
+    GamePanel gp;
+
+    public AssetSetter(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public void setObject() {
+
+        gp.obj[0] = new OBJ_Key();
+        gp.obj[0].worldX = 10 * gp.tileSize;
+        gp.obj[0].worldY = 50 * gp.tileSize;
+
+        gp.obj[1] = new OBJ_Key();
+        gp.obj[1].worldX = 31 * gp.tileSize;
+        gp.obj[1].worldY = 14 * gp.tileSize;
+
+    }
+}
